37869273a0e6aa0c081ddd33022e3fa4fa4fb56f
Arvind Cletus <arvindcletus@gmail.com>
2022-02-02 21:39:34 +0530

    Created a route to get a single post.

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -7,6 +7,8 @@ import pytest
 
 
 def test_create_summary(test_app_with_db):
+    """Test to check for a valid POST request"""
+
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar"}))
 
@@ -15,6 +17,8 @@ def test_create_summary(test_app_with_db):
 
 
 def test_create_summaries_invalid_json(test_app):
+    """Test to check for a invalid POST request"""
+
     response = test_app.post("/summaries/", data=json.dumps({}))
     assert response.status_code == 422
     assert response.json() == {
@@ -26,3 +30,28 @@ def test_create_summaries_invalid_json(test_app):
             }
         ]
     }
+
+
+def test_read_summary(test_app_with_db):
+    """Test to get a single summary route"""
+
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.get(f"/summaries/{summary_id}/")
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"]
+    assert response_dict["created_at"]
+
+
+def test_read_summary_incorrect_id(test_app_with_db):
+    """Test to check for an incorrect id"""
+
+    response = test_app_with_db.get("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
